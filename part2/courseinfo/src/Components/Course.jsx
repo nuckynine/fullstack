625802ed6dc33c9d246ef2ee55af1fa7508ecbf7
nuckynine <91093370+nuckynine@users.noreply.github.com>
2024-09-08 01:59:42 -0600

    Mapping

---
+++ part2/courseinfo/src/Components/Course.jsx
@@ -0,0 +1,42 @@
+﻿const Header = (props) => {
+    return <h1>{props.name}</h1>
+}
+
+const Part = (data) => {
+    return (
+        <p>
+            {data.name} {data.num}
+        </p>
+    )
+}
+const Content = (props) => {
+    return (
+        <div>
+            {props.parts.map(data =>
+                <Part key={data.id} name={data.name} num={data.exercises}/>
+            )}
+        </div>
+    )
+}
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.parts.reduce((sum, part) => {
+            return sum + part.exercises
+        }, 0)}
+        </p>)
+}
+
+
+const Course = ({courses}) => {
+    return (
+        <>
+            <Header name={courses[0].name}/>
+            <Content parts={courses[0].parts}/>
+            <Total parts={courses[0].parts}/>
+            <Header name={courses[1].name}/>
+            <Content parts={courses[1].parts}/>
+            <Total parts={courses[1].parts}/>
+        </>
+    )
+}
+export default Course
---
+++ part2/src/Components/Note.jsx
@@ -0,0 +1,6 @@
+﻿const Note = ({note}) => {
+    return (
+        <li>{note.content}</li>
+    )
+}
+export default Note
